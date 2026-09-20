8dee659e45b8888f84d21d802459c86a2c9d7233
enggub6-lgtm <enggub6@gmail.com>
2026-09-20 11:16:08 +0300

    Remove old website files

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  const btn=document.querySelector('.menu-btn');
-  const nav=document.querySelector('.nav-links');
-  if(btn && nav){
-    btn.addEventListener('click',()=>nav.classList.toggle('open'));
-  }
-});
